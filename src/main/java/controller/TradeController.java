ae4b6c4ed2fc31519ab04326fea1b3794eb2ff81
shivanshnagarkar <shivanshunagarkar@gmail.com>
2024-11-07 02:12:02 +0530

    adding correct response to controller

--- src/main/java/controller/TradeController.java
+++ src/main/java/controller/TradeController.java
@@ -10,9 +10,7 @@ import model.Trade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.TradeService;
 
 @RestController()
@@ -34,6 +32,24 @@ public class TradeController {
             return new ResponseEntity<Response>(response, HttpStatus.UNPROCESSABLE_ENTITY);
         } catch (InsufficientQuantityException e) {
             response = new Response(trade.getTradeId()==null?-1:trade.getTradeId(), Status.REJECTED);
+            return new ResponseEntity<Response>(response, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        return new ResponseEntity<Response>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/create/{id}")
+    public ResponseEntity<Response> createOrUpdateTrade(@RequestBody Trade trade, @PathVariable Integer tradeId)  {
+        Response response = null;
+        try {
+            response = tradeService.createOrUpdateTrade(trade);
+        } catch (InvalidTradeException e) {
+            response = new Response(tradeId, Status.REJECTED);
+            return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);
+        } catch (InsufficientCashException e) {
+            response = new Response(tradeId, Status.REJECTED);
+            return new ResponseEntity<Response>(response, HttpStatus.UNPROCESSABLE_ENTITY);
+        } catch (InsufficientQuantityException e) {
+            response = new Response(tradeId, Status.REJECTED);
             return new ResponseEntity<Response>(response, HttpStatus.UNPROCESSABLE_ENTITY);
         }
         return new ResponseEntity<Response>(response, HttpStatus.OK);
