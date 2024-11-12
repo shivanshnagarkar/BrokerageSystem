8e36f6721a77d0e1e09f9e598e81f7c7556d841e
shivanshnagarkar <34064139+shivanshnagarkar@users.noreply.github.com>
2024-11-12 13:28:01 +0530

    Update TradeController.java

--- src/main/java/controller/TradeController.java
+++ src/main/java/controller/TradeController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.TradeService;
 
-@RestController()
+@RestController
 @RequestMapping("/trades")
 public class TradeController {
 
